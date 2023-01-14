21b642f69dbfb986f396d95680f11b58852d5d76
TOBZi <81484769+ArbawiStudio@users.noreply.github.com>
2023-01-14 11:19:08 +0200

    ThxBot

---
+++ Thx.js
@@ -0,0 +1,9 @@
+const { Client, Partials } = require('discord.js')
+
+const client = new Client({
+    partials: [
+        Partials.Channel,
+        Partials.Message,
+        Partials.
+    ]
+})
