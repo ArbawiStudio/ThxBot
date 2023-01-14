591b7461d092be829f3e5ea528953640f3f08e6f
TOBZi <81484769+ArbawiStudio@users.noreply.github.com>
2023-01-14 11:20:31 +0200

    ThxBot

--- Thx.js
+++ Thx.js
@@ -1,9 +1,17 @@
-const { Client, Partials } = require('discord.js')
+const { Client, Partials, IntentsBitField } = require('discord.js')
 
 const client = new Client({
     partials: [
         Partials.Channel,
         Partials.Message,
-        Partials.
+        Partials.User
+    ],
+
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
     ]
 })
+
+require('fs').readdir()
